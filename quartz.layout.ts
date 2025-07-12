489006af3d80efeba4a5dce4016e54da4376b9ff
snowiow <marcel.patzwahl@posteo.de>
2025-07-12 14:12:21 +0200

    commit build page as well

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,8 +38,8 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
     Component.RecentNotes(),
+    Component.Explorer(),
   ],
   right: [
     Component.Graph(),
